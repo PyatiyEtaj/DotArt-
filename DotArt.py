0ce72bbe1a108a3b2c7467dd63668559cf5e5b9d
PyatiyEtaj <pyatiyEtaj@gmail.com>
2020-07-31 12:47:21 +0400

    refactoring

--- DotArt.py
+++ DotArt.py
@@ -9,7 +9,7 @@ tb_scale = 'width'
 tb_type = 'type'
 title_window="OPTIONS"
 
-def get_symbol(img, i, j):
+def color_to_utf8(img, i, j):
     (h, w) = img.shape
     symbol = 0 
     if (i+3 < h and j+1 < w):
@@ -26,16 +26,16 @@ def get_symbol(img, i, j):
     if (EMPTY_SPACES and symbol == 0) : symbol = 1
     return chr(10240 + symbol)
 
-def translate_cv2(img):
+def to_ascii(img, color_to_symbol, step_i = 1, step_j = 1):
     (h, w) = img.shape
-    ASCII_art = ""
-    for i in range(0, h, 4):
-        for j in range(0, w, 2):
-            ASCII_art += get_symbol(img, i,j)
-        ASCII_art += '\n'
-    return ASCII_art
+    ascii_art = ""
+    for i in range(0, h, step_i): # 4
+        for j in range(0, w, step_j): # 2
+            ascii_art += color_to_symbol(img, i,j)
+        ascii_art += '\n'
+    return ascii_art
 
-def img_size(img, scale): 
+def rescaling(img, scale): 
     width = img.shape[1] * scale / 100
     height = img.shape[0] * scale / 100
     width -= width%4
@@ -43,22 +43,22 @@ def img_size(img, scale):
     return (int(width), int(height)) 
 
 def resize_img(img, scale):            
-    sz = img_size(img, scale)
-    resize = cv2.resize(img, sz, interpolation = cv2.INTER_AREA)
-    return resize
+    sz = rescaling(img, scale)
+    resized = cv2.resize(img, sz, interpolation = cv2.INTER_AREA)
+    return resized
 
 def do_nothing(val):
     pass
 
-def init_wnd():
+def init_wnds():
     cv2.namedWindow(title_window)
     cv2.resizeWindow(title_window, 384, 90)
     cv2.createTrackbar(tb_threshold,  title_window, 0, 255, do_nothing)
     cv2.createTrackbar(tb_scale,  title_window, 100, 100, do_nothing)
     cv2.createTrackbar(tb_type,  title_window, 0, 1, do_nothing)
 
-def cv_2_wnd_threshold(img):    
-    init_wnd()
+def image_setup(img):    
+    init_wnds()
     info = cv2.resize(img, (384,120), interpolation = cv2.INTER_AREA)
     info[:] = 255
     result = img
@@ -71,18 +71,29 @@ def cv_2_wnd_threshold(img):
         switch  = cv2.THRESH_BINARY if cv2.getTrackbarPos(tb_type,  title_window) > 0.5 else cv2.THRESH_BINARY_INV
         _, result = cv2.threshold(result, val, 255, switch)
 
-        info = cv2.putText(info, f'{result.shape[0]}, {result.shape[1]}, {result.shape[0]*result.shape[1]/8}', (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, 0)
+        info = cv2.putText(info, f'h={result.shape[0]}, w={result.shape[1]}, s=~{int(result.shape[0]*result.shape[1]/8)}', (10,50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, 0)
         cv2.imshow('IMAGE', result) 
         cv2.imshow(title_window, info) 
         info[:] = 255
         cv2.resizeWindow('IMAGE', result.shape[1], result.shape[0])
         k = cv2.waitKey(1) & 0xFF        
         if (k == 27):
-            break;
-        
+            break;        
 
     cv2.destroyAllWindows()
     return result
+
+def read_image(path):
+    img = cv2.imread(path)    
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img
+
+def output_ascii_art(ascii_art, constraints, outputfile):
+    if (len(ascii_art) < constraints):
+            print(ascii_art)
+    if (not outputfile is None):
+        with open(outputfile, "w", encoding="UTF-8") as text_file:
+            print(ascii_art, file=text_file)
 
 if __name__ == "__main__":
     usage = 'usage: %prog [options]'
@@ -106,13 +117,8 @@ if __name__ == "__main__":
     #print(f'[-i = {inputfile}] [-o = {outputfile}] [-c = {constraints}] [-es = {EMPTY_SPACES}]\n')
 
     if (not inputfile is None):        
-        img = cv2.imread(inputfile)    
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv_2_wnd_threshold(img)
-        art = translate_cv2(img)
-        if (len(art) < constraints):
-            print(art)
-        if (not outputfile is None):
-            with open(outputfile, "w", encoding="UTF-8") as text_file:
-                print(art, file=text_file)
+        img = read_image(inputfile)
+        img = image_setup(img)
+        ascii_art = to_ascii(img, color_to_utf8, 4, 2)
+        output_ascii_art(ascii_art, constraints, outputfile)
     
